cf11babae2cb37251c74bad4b5a0c24c896e3a8e
Jorge Benavides Nati <jbenavides@sistecredito.com>
2023-01-25 11:05:13 -0500

    "corregido"

--- define.c
+++ define.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
-#define PIZZACOST = 15;
+// #define PIZZACOST = 15;
+const char NEWLINE = '\n';
 
 int main(){
 
-    float costoPizzas;
+    float costoPizzas = 0;
+    float pizzaCost = 5;
     float numberOfSlices = 3;
-    costoPizzas = PIZZACOST * numberOfSlices;
-    printf("total bill %f", costoPizzas);
-    printf(newLine);
-
+    costoPizzas = pizzaCost * numberOfSlices;
+    printf("total bill: %f", costoPizzas);
 }
