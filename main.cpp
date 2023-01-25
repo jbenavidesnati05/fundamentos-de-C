793d7549cc52ec6641fd4152ce413af1e46ac017
Jorge Benavides Nati <jbenavides@sistecredito.com>
2023-01-24 20:20:35 -0500

    first commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdio.h>
+
+int main(){
+    printf("hello platzi");
+    return 0;
+}
+
+// tipos de datos
+// Basic types: 
+// 1- aritmeticos           => int 
+// 2- de punto flotante     
